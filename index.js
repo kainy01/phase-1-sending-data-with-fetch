bec1e41418e33be2add4b4d2ce4cc7bd79e7bd7a
kainy01 <kainykawai@gmail.com>
2023-05-10 16:23:27 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// method: "POST" is missing from the object below
+
+function submitData(name, email) {
+  const formData = { name: name, email: email };
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.body.innerHTML = data["id"]
+  })
+  .catch(error => {
+    document.body.innerHTML = error.message
+  });
+}
+
+submitData("zain", "hshs");
